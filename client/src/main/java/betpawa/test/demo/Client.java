6e0b144b08f5f816ce2454d431c7e1ffe6c16d0a
desert5 <nova5@email.com>
2020-01-04 03:25:41 +0200

    Client converted to boot application

--- client/src/main/java/betpawa/test/demo/Client.java
+++ client/src/main/java/betpawa/test/demo/Client.java
@@ -2,22 +2,67 @@ package betpawa.test.demo;
 
 import betpawa.test.demo.grpc.*;
 import com.google.common.util.concurrent.FluentFuture;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-public class Client
-{
-    public static void main(String[] args)
-    {
+import static java.util.Arrays.asList;
+
+@SpringBootApplication
+public class Client implements ApplicationRunner {
+
+    @Autowired
+    private ApplicationContext context;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Client.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) {
+        Integer roundsPerThread = 10;
+        Integer threadsPerUser = 10;
+
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9999).usePlaintext().build();
         WalletServiceGrpc.WalletServiceFutureStub service = WalletServiceGrpc.newFutureStub(channel);
-        ExecutorService executor = Executors.newFixedThreadPool(10);
+        ExecutorService executor = Executors.newFixedThreadPool(threadsPerUser);
+        Random random = new Random();
+
         Long userId = 1L;
 
-        FluentFuture<PaymentResponse> roundA = roundA(service, executor, userId);
+        List<Callable<FluentFuture<?>>> rounds = asList(
+                () -> roundA(service, executor, userId),
+                () -> roundB(service, executor, userId),
+                () -> roundC(service, executor, userId));
+
+        ArrayList<ListenableFuture<?>> join = new ArrayList<>();
+
+        executor.submit(() -> {
+            List<? extends FluentFuture<?>> futures = IntStream.range(0, roundsPerThread)
+                    .mapToObj((i) -> rounds.get(random.nextInt(rounds.size())))
+                    .map((fn) -> fn.call())
+                    .collect(Collectors.toList());
+
+            join.add(Futures.allAsList(futures));
+        });
+
+        Futures.whenAllComplete(join).call(() -> SpringApplication.exit(context, () -> 0), executor);
     }
 
     private static FluentFuture<PaymentResponse> roundA(WalletServiceGrpc.WalletServiceFutureStub service, ExecutorService e, Long userId) {
