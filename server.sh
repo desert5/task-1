e577852fde8d2223383db5d8085f5f099282a834
desert5 <nova5@email.com>
2020-01-06 03:28:35 +0200

    Fixed docker setup and added readme

--- server.sh
+++ server.sh
@@ -1,3 +1,2 @@
-# Works only with Java 8 because of javax.annotation.Generated used by grpc generator, so jdk installation needs to be specified explicitly
 ./gradlew clean build #-Dorg.gradle.java.home=/usr/lib/jvm/java-8-openjdk-amd64
-docker-compose up
+docker-compose up --build
